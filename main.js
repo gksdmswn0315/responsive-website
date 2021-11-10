d0bd637bb66a139f4f782a7892e93eaffbecd125
gksdmswn0315 <haneunju020315@gmail.com>
2021-11-10 13:57:00 +0900

    .

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+​
+//넷바 색상 변경
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+​
+});
+​
+//넷바메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+​
+//contact me 버튼 이동하기
+const contactMeBtn = document.querySelector(".home__contact");
+contactMeBtn.addEventListener("click", ()=> {
+    scrollIntoView("#contact");
+});
+​
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+    }
