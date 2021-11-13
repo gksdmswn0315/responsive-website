2929cef0fad3ae1fc5bc6b7c15fb32648cb8e75b
gksdmswn0315 <haneunju020315@gmail.com>
2021-11-13 16:07:34 +0900

    .

--- main.js
+++ main.js
@@ -1,35 +1,50 @@
 'use strict';
-​
+
 //넷바 색상 변경
-const navbar = document.querySelector('#navbar');
+// let :변하는 상수 | const :변하지 않는 상수
+const navbar =document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-    if(window.scrollY > navbarHeight){
+    if(window. scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
-​
+
 });
-​
+
 //넷바메뉴 선택시 해당 부분으로 이동하기
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener("click", (event)=>{
     const target = event.target;
     const link = target.dataset.link;
-    if (link == null) {
+    if(link == null) {
         return;
     }
+    navbarMenu.classList.remove("open");
     scrollIntoView(link);
 });
-​
+
 //contact me 버튼 이동하기
 const contactMeBtn = document.querySelector(".home__contact");
 contactMeBtn.addEventListener("click", ()=> {
-    scrollIntoView("#contact");
+    scrollIntoView('#contact');
 });
-​
+
+//home 투명도 조절
+const home = document.querySelector(".home__container")
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", ()=>{
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+});
+
+//햄버거 메뉴 클릭할 때
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", ()=>{
+    navbarMenu.classList.toggle("open");
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-    }
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
